7c759d100aa5d2a8286815671681666ecf0a2177
nailyk <nailyk_git@nailyk.fr>
2018-02-19 14:40:26 +0100

    [WIP] Allow 8960 to use 8974 HAL
    Change-Id: I39eaad9606d0068b320204a4bb1068f98fa2e172
    Signed-off-by: nailyk <nailyk_git@nailyk.fr>

--- Android.mk
+++ Android.mk
@@ -8,11 +8,6 @@ display-hals += liblight
 endif
 ifeq ($(call is-vendor-board-platform,QCOM),true)
     include $(call all-named-subdir-makefiles,$(display-hals))
-else
-ifneq ($(filter msm8960,$(TARGET_BOARD_PLATFORM)),)
-    #This is for mako since it doesn't have the QCOM make functions
-    include $(call all-named-subdir-makefiles,$(display-hals))
-endif
 endif
 
 endif
